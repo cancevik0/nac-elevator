006f5130f76c9aafd1672937d63c294c2d0acea9
Can Çevik <136027261+cancevik0@users.noreply.github.com>
2025-02-18 06:52:25 +0300

    Delete locales/tr.lua

--- locales/tr.lua
+++
@@ -1,7 +0,0 @@
-Locales = Locales or {}  -- Eğer Locales tablosu tanımlı değilse, onu tanımlar
-
-Locales['tr'] = {
-    ['use_elevator'] = 'Asansörü Kullan',
-    ['go_to_floor'] = 'Bu kata git',
-    ['elevator_floors'] = 'Asansör Katları',
-}
