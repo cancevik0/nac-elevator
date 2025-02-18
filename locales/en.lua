e2d03072c5c5db92ed5bcf9c438221ef920726f3
Can Çevik <136027261+cancevik0@users.noreply.github.com>
2025-02-18 06:51:39 +0300

    en.lua

---
+++ locales/en.lua
@@ -0,0 +1,7 @@
+Locales = Locales or {}  -- Eğer Locales tablosu tanımlı değilse, onu tanımlar
+
+Locales['en'] = {
+    ['use_elevator'] = 'Use Elevator',
+    ['go_to_floor'] = 'Go to this floor',
+    ['elevator_floors'] = 'Elevator Floors',
+}
