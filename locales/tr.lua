409fd9a87e17882dc8c4b64943af7310ac8033e4
Can Çevik <136027261+cancevik0@users.noreply.github.com>
2025-02-18 06:51:52 +0300

    tr.lua

---
+++ locales/tr.lua
@@ -0,0 +1,7 @@
+Locales = Locales or {}  -- Eğer Locales tablosu tanımlı değilse, onu tanımlar
+
+Locales['tr'] = {
+    ['use_elevator'] = 'Asansörü Kullan',
+    ['go_to_floor'] = 'Bu kata git',
+    ['elevator_floors'] = 'Asansör Katları',
+}
